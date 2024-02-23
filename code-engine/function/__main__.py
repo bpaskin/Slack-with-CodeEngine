38ea4603b4af7fcd864d3c6fd1c36d2c934c7c0b
Brian S Paskin <21156194+bpaskin@users.noreply.github.com>
2024-02-23 07:25:13 -0500

    Add other fields/time boxed to 1 year

--- code-engine/function/__main__.py
+++ code-engine/function/__main__.py
@@ -3,9 +3,21 @@
 import logging
 import os
 import json 
+import datetime
 
 from slack_sdk import WebClient
 from slack_sdk.errors import SlackApiError
+
+def count_reactions(REACTIONS):
+   REACTION_COUNT = 0
+   for REACTION in REACTIONS:
+      REACTION_COUNT +=  REACTION['count'] 
+   return REACTION_COUNT 
+
+def convertDate(TIMESTAMP):
+   FL_TIMESTAMP = float(TIMESTAMP)
+   obj = datetime.datetime.fromtimestamp(FL_TIMESTAMP, datetime.timezone.utc)
+   return obj.strftime("%Y-%m-%d, %H:%M:%S %z")
 
 def main(params):
 
@@ -36,7 +48,8 @@ def main(params):
    client = WebClient(SLACK_TOKEN)
 
    try:
-      result = client.conversations_history(channel=CHANNEL_ID)
+      oneYearAgo = datetime.datetime.now() - datetime.timedelta(365)
+      result = client.conversations_history(channel=CHANNEL_ID,oldest=oneYearAgo.timestamp())
       MESSAGES = result["messages"]
 
       # Loop through the messages and get only true messages and not joins, leaves, etc
@@ -51,19 +64,32 @@ def main(params):
 
                # need to loop through the replies
                for REPLY in REPLIES['messages']:
+                  DATE = convertDate(REPLY['ts'])
                   # This is top message
                   if REPLY['ts'] == REPLY['thread_ts']:
-                     ITEM = {"timestamp": REPLY['ts'], "message": REPLY['text']}
+                     REACTION_COUNT = 0
+                     if 'reactions' in REPLY:
+                        REACTION_COUNT = count_reactions(REPLY['reactions'])
+                     MSG = "Idea - " + REPLY['text']
+                     ITEM = {"timestamp": REPLY['ts'], "date": DATE, "message": MSG, "reactions": REACTION_COUNT }
                   # These are the replies
                   else:
-                     ITEM1 = {"timestamp": REPLY['ts'], "message": REPLY['text']} 
+                     REACTION_COUNT = 0
+                     if 'reactions' in REPLY:
+                        REACTION_COUNT = count_reactions(REPLY['reactions'])
+                     ITEM1 = {"timestamp": REPLY['ts'],  "date": DATE, "message": REPLY['text'], "reactions": REACTION_COUNT} 
                      RPS.append(ITEM1)
 
                ITEM['replies'] = RPS 
                OUTPUT.append(ITEM)
 
             else:
-               ITEM = {"timestamp": MESSAGE['ts'], "message": MESSAGE['text']}
+               REACTION_COUNT = 0
+               if 'reactions' in MESSAGE:
+                  REACTION_COUNT = count_reactions(MESSAGE['reactions'])
+               DATE = convertDate(MESSAGE['ts'])
+               MSG =  "Idea - " + MESSAGE['text']
+               ITEM = {"timestamp": MESSAGE['ts'], "date": DATE, "message": MSG, "reactions": REACTION_COUNT}
                OUTPUT.append(ITEM)
 
       logger.info(OUTPUT)
